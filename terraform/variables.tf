abd3e9ab9d52d883643a018027753dcd2bca3f44
sagar n mutalik <sagarnmutalik6@gmailcom>
2026-08-29 15:40:32 +0530

    Update configurations and switch to t3.micro

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.small" # t3.small is good for a simple web app. Update to t3.medium if running out of memory during builds.
+  default     = "t3.micro" # t3.micro is cost-effective for simple apps.
 }
 
 variable "key_name" {
